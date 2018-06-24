8d364aa059d3197478fc6871fd2d94fbf06b4156
Ben <ishmandoo@gmail.com>
2018-06-24 19:24:58 -0400

    pushing trash

--- pytorch_q.py
+++ pytorch_q.py
@@ -20,6 +20,7 @@ from collections import deque
 
 import matplotlib.pyplot as plt
 
+#plt.ion()
 
 Variable = autograd.Variable
 
@@ -46,11 +47,11 @@ class DQN(nn.Module):
         self.num_actions = num_actions
         
         self.layers = nn.Sequential(
-            nn.Linear(num_inputs, 128),
+            nn.Linear(num_inputs, 4),
             nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, num_actions)
+            #nn.Linear(2, 2),
+            #nn.ReLU(),
+            nn.Linear(4, num_actions)
         )
         
     def forward(self, x):
@@ -67,12 +68,12 @@ class DQN(nn.Module):
         return action
 # theta, thetadot, x, xdot = state
 # left right = actios
-model = DQN(5, 2)
+model = DQN(2, 2)
 
     
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+optimizer = optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.001)
 
-replay_buffer = ReplayBuffer(1000)
+replay_buffer = ReplayBuffer(5000)
 
 
 def compute_td_loss(batch_size):
@@ -96,6 +97,9 @@ def compute_td_loss(batch_size):
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
+
+    for param in model.parameters():
+        print(param.data)
     
     return loss
 
@@ -122,7 +126,7 @@ cart.zeroAnalyzer()
 cart.goTo(500)
 
 batch_size = 32
-gamma      = 0.99
+gamma      = 0.95
 speed = 1000
 
 losses = []
@@ -130,27 +134,58 @@ all_rewards = []
 episode_reward = 0
 
 def get_reward(state):
-    x,x_dot,cos_theta, sin_theta ,theta_dot = state
-    return -cos_theta + 1.0
+    x = state[0]#,x_dot,cos_theta, sin_theta ,theta_dot = state
+    '''
+    if state[0] < .1:
+        return -1
+    if state[0] > .8:
+        return -1
+    '''
+    return -1 * (x)**2 # -cos_theta + 1.0 
 
 def expand_state(state):
-    return [state[0], state[1], np.cos(state[2]), np.sin(state[2]), state[3]]
+    return [state[0]/1000 - 0.5 , state[1]/1000.]#, 0, 0, state[3]] # np.cos(state[2])np.sin(state[2])
 
-def plot(frame_idx, rewards, losses):
+def plot(frame_idx, rewards, losses, model):
     #clear_output(True)
-    plt.figure(figsize=(20,5))
-    plt.subplot(131)
+    plt.clf()
+    plt.figure(1, figsize=(20,5))
+    plt.subplot(141)
     plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
     plt.plot(rewards)
-    plt.subplot(132)
+    plt.subplot(142)
     plt.title('loss')
     plt.plot(losses)
-    plt.show()
 
-epsilon_start = 1.0
+    plt.subplot(143)
+    plt.title('q')
+    qls = []
+    qrs = []
+    diffs = []
+    xs = np.arange(-0.3, 0.4, 0.1)
+    for x in xs:
+        out = model.forward(torch.tensor([x, 0]).unsqueeze(0))
+        print("out", out.detach().numpy()[0])
+        ql, qr = out.detach().numpy()[0]
+        qls.append(ql)
+        qrs.append(qr)
+        diffs.append(ql-qr)
+    plt.plot(xs,qls)
+    plt.plot(xs,qrs)    
+
+    plt.subplot(144)
+    plt.title('q diff')
+    plt.plot(xs,diffs)  
+
+    plt.pause(0.01)
+    #plt.show()
+
+epsilon_start = 1.2
 epsilon_final = 0.01
-epsilon_decay = 500
+epsilon_decay = 3000
 epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
+
+cart_speed = 0
 
 i = 0
 all_rewards = []
@@ -158,19 +193,27 @@ while True:
     i += 1
     state = cart.analyzer.getState()
     state = expand_state(state)
+    print("state", state)
     epsilon = epsilon_by_frame(i)
     action = model.act(state, epsilon)
     
-    cart.setSpeedMmPerS(1000 * ((2 * action) -1))
-    if not 100 < state[0] < 800:
-        cart.setSpeedMmPerS(0)
+    cart_speed = 1000 * ((2 * action) -1)
+    cart.setSpeedMmPerS(cart_speed)
+
+    if state[0] < -.3:
+        cart.setSpeedMmPerS(800)
+        cart_speed = 800
+        continue
+    if state[0] > .2:
+        cart.setSpeedMmPerS(-800)
+        cart_speed = -800
         continue
 
     next_state = cart.analyzer.getState()    
     next_state = expand_state(next_state)
     reward = get_reward(next_state)
     print(reward, epsilon, action)
-    done = False
+    done = 0
 
     replay_buffer.push(state, action, reward, next_state, done)
     
@@ -187,5 +230,5 @@ while True:
         loss = compute_td_loss(batch_size)
         losses.append(loss.data[0])
         
-    if i % 2000 == 0:
-        plot(i, all_rewards, losses)
+    if i % 20 == 0:
+        plot(i, all_rewards, losses, model)
